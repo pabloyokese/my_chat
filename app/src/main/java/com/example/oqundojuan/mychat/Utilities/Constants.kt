4321f56fcde25a5d5a0a36e89bdbb57a8d1012a2
Juan <oqundo.juan@system-in-motion.com>
2019-03-12 17:45:12 +0800

    adding login user logic

--- app/src/main/java/com/example/oqundojuan/mychat/Utilities/Constants.kt
+++ app/src/main/java/com/example/oqundojuan/mychat/Utilities/Constants.kt
@@ -2,3 +2,4 @@ package com.example.oqundojuan.mychat.Utilities
 
 const val BASE_URL = "http://192.168.0.106:3000/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
