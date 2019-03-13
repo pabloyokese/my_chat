760e49cd5568e30464cfdbcd6c370cd3509a5027
Juan <oqundo.juan@system-in-motion.com>
2019-03-13 17:45:20 +0800

    adding function to find user by email

--- app/src/main/java/com/example/oqundojuan/mychat/controller/LoginActivity.kt
+++ app/src/main/java/com/example/oqundojuan/mychat/controller/LoginActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.oqundojuan.mychat.R
+import com.example.oqundojuan.mychat.services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -20,6 +22,18 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view:View){
-
+        val email = loginEmailTxt.text.toString()
+        val password = loginPasswordTxt.text.toString()
+        AuthService.loginUser(this,email,password){
+            loginSuccess ->
+            if (loginSuccess){
+                AuthService.findUserByEmail(this){
+                    findSuccess->
+                        if(findSuccess){
+                            finish()
+                        }
+                }
+            }
+        }
     }
 }
