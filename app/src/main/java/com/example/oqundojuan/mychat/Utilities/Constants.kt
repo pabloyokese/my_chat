2b14792a18984a3ec41bc538fa08629717709d69
Juan <oqundo.juan@system-in-motion.com>
2019-03-13 14:47:02 +0800

    adding some refactoring

--- app/src/main/java/com/example/oqundojuan/mychat/Utilities/Constants.kt
+++ app/src/main/java/com/example/oqundojuan/mychat/Utilities/Constants.kt
@@ -3,3 +3,7 @@ package com.example.oqundojuan.mychat.Utilities
 const val BASE_URL = "http://192.168.0.106:3000/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
+
+// broadcast constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
---
+++ app/src/main/java/com/example/oqundojuan/mychat/services/UserDatService.kt
@@ -0,0 +1,10 @@
+package com.example.oqundojuan.mychat.services
+
+object UserDataService{
+    var id = ""
+    var avatarColor = ""
+    var avatarName = ""
+    var email = ""
+    var name = ""
+
+}
