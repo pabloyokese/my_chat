2563c2aaeadbc54f3ffd726c6bb7732c0015e451
Juan <oqundo.juan@system-in-motion.com>
2019-03-15 14:03:34 +0800

    adding features for login activities

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,9 +1,15 @@
 
-o
-?app/src/main/java/com/example/oqundojuan/mychat/MainActivity.kt,6\a\6afb99e172efa0b09a6c376ecb73f7cd5785c073
 P
  app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
-p
-@app/src/main/java/com/example/oqundojuan/mychat/LoginActivity.kt,3\b\3be1bf65ee63c6466fddf9467d49ff2b4545e350
-u
-Eapp/src/main/java/com/example/oqundojuan/mychat/CreateUserActivity.kt,a\9\a9b60fb0c5c8f267d3c633687e025669552edc1b
+v
+Fapp/src/main/java/com/example/oqundojuan/mychat/Utilities/Constants.kt,b\7\b7cf837b8af6557727796c7d912bb391d7ffd74f
+w
+Gapp/src/main/java/com/example/oqundojuan/mychat/services/AuthService.kt,0\a\0af3b38842c0de27a58af4d6cf420a2dee451978
+z
+Japp/src/main/java/com/example/oqundojuan/mychat/services/UserDatService.kt,f\e\fee92683b7be64f15818befd486a5766057fbbab
+{
+Kapp/src/main/java/com/example/oqundojuan/mychat/controller/LoginActivity.kt,2\1\211eb309171fff3c07bb7e3f163e057f17c88ab0
+€
+Papp/src/main/java/com/example/oqundojuan/mychat/controller/CreateUserActivity.kt,3\b\3bfff1db13fdfd1c397fa85e107c06e8e90d59c8
+z
+Japp/src/main/java/com/example/oqundojuan/mychat/controller/MainActivity.kt,c\3\c3ae180c00e8257f97c2bc2bd833635713c11fd3
--- app/src/main/java/com/example/oqundojuan/mychat/controller/LoginActivity.kt
+++ app/src/main/java/com/example/oqundojuan/mychat/controller/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.example.oqundojuan.mychat.controller
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.oqundojuan.mychat.R
 import com.example.oqundojuan.mychat.services.AuthService
 import kotlinx.android.synthetic.main.activity_login.*
@@ -13,6 +16,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginCreateUserBtnClicked(view: View){
@@ -22,18 +27,54 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view:View){
+        enableSpinner(true)
         val email = loginEmailTxt.text.toString()
         val password = loginPasswordTxt.text.toString()
-        AuthService.loginUser(this,email,password){
-            loginSuccess ->
-            if (loginSuccess){
-                AuthService.findUserByEmail(this){
-                    findSuccess->
+        hideKeyboard()
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.loginUser(this,email,password){
+                    loginSuccess ->
+                if (loginSuccess){
+                    AuthService.findUserByEmail(this){
+                            findSuccess->
                         if(findSuccess){
+                            enableSpinner(false)
                             finish()
+                        }else{
+                            errorToast()
                         }
+                    }
+                }else{
+                    errorToast()
                 }
             }
+        }else{
+            Toast.makeText(this, "Please fill in both email and password", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
+        }
+    }
+
+    fun errorToast(){
+        Toast.makeText(this,"Something went wrong pleae, try again",
+            Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    private fun enableSpinner(enable: Boolean){
+        if(enable){
+            loginSpinner.visibility = View.VISIBLE
+        }else{
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+    }
+
+    fun hideKeyboard(){
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if(inputManager.isAcceptingText){
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken,0)
         }
     }
 }
