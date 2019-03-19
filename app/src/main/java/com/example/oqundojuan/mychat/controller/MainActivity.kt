766b4200ff553a4ceb4b693c358f9b246df353b7
Juan <oqundo.juan@system-in-motion.com>
2019-03-19 13:56:09 +0800

    adding message adapter

--- app/src/main/java/com/example/oqundojuan/mychat/controller/MainActivity.kt
+++ app/src/main/java/com/example/oqundojuan/mychat/controller/MainActivity.kt
@@ -12,11 +12,13 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import com.example.oqundojuan.mychat.Adapters.MessageAdapter
 import com.example.oqundojuan.mychat.Model.Channel
 import com.example.oqundojuan.mychat.Model.Message
 import com.example.oqundojuan.mychat.services.MessageService
@@ -36,12 +38,17 @@ class MainActivity : AppCompatActivity(){
 
     val socket = IO.socket(SOCKET_URL)
     lateinit var channelAdapter: ArrayAdapter<Channel>
+    lateinit var messageAdapter: MessageAdapter
     var selectedChannel: Channel? = null
 
     private fun setupAdapters(){
         channelAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,
             MessageService.channels)
         channel_list.adapter = channelAdapter
+        messageAdapter = MessageAdapter(this,MessageService.messages)
+        messageListView.adapter = messageAdapter
+        val layoutManager = LinearLayoutManager(this)
+        messageListView.layoutManager = layoutManager
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -116,8 +123,9 @@ class MainActivity : AppCompatActivity(){
             MessageService.getMessages(selectedChannel!!.id){
                 complete->
                     if(complete){
-                        for (message in MessageService.messages){
-                            println(message.message)
+                        messageAdapter.notifyDataSetChanged()
+                        if(messageAdapter.itemCount > 0){
+                            messageListView.smoothScrollToPosition(messageAdapter.itemCount - 1)
                         }
                     }
             }
@@ -136,6 +144,8 @@ class MainActivity : AppCompatActivity(){
         if(App.prefs.isLoggedIn){
             // log out
             UserDataService.logout()
+            channelAdapter.notifyDataSetChanged()
+            messageAdapter.notifyDataSetChanged()
             nav_drawer_header_include.userNameNavHeader.text = ""
             nav_drawer_header_include.userEmailNavHeader.text = ""
             nav_drawer_header_include.userImageNavHeader.setImageResource(R.drawable.profiledefault)
@@ -200,6 +210,8 @@ class MainActivity : AppCompatActivity(){
 
                     val newMessage = Message(msgBody,userName,channelId,userAvatar,userAvatarColor,id,timeStamp)
                     MessageService.messages.add(newMessage)
+                    messageAdapter.notifyDataSetChanged()
+                    messageListView.smoothScrollToPosition(messageAdapter.itemCount - 1)
                 }
             }
         }
